287849f4620fc64e72453f42bea005a3a6ab3b9b
Steffan <skbrown333@gmail.com>
2019-06-10 17:53:51 -0500

    setup mono repo

--- packages/bot/src/bot.ts
+++ packages/bot/src/bot.ts
@@ -1,10 +1,8 @@
 import { IEXService } from "@stock-bot/modules";
-console.log("IEXService: ", IEXService);
 
 async function test() {
-  let s = await new IEXService().getSymbolQuote("aapl", null);
+  let s = await IEXService.getSymbolQuote("aapl", null);
   console.log(s);
 }
 
-console.log("Hello World");
 test();
